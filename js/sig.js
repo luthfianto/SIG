8f41c4a6b05df7713ac9b1c59554daf97cac6c1d
Rizky Luthfianto <mrluthfianto@gmail.com>
2015-12-16 20:56:45 +0700

    Use Vue.js

--- js/sig.js
+++ js/sig.js
@@ -1,21 +1,16 @@
 // Global map variable
 var map;
 
-// Set the center as Firebase HQ
-// var locations = {
-//   "Milan": [-7.7699922,110.3725859],
-//   "Pogung": [-7.7620407,110.3715559]
-// };
 var center = [-6,120]
 
 // Query radius
 var radiusInKm = 0.5;
 
 // Get a reference to the Firebase public transit open data set
-var transitFirebaseRef = new Firebase("https://torrid-inferno-2232.firebaseio.com/")
+// var transitFirebaseRef = new Firebase("https://torrid-inferno-2232.firebaseio.com/")
 
 // Create a new GeoFire instance, pulling data from the public transit data
-var geoFire = new GeoFire(transitFirebaseRef.child("_geofire"));
+// var geoFire = new GeoFire(transitFirebaseRef.child("_geofire"));
 
 /*************/
 /*  GEOQUERY */
@@ -23,10 +18,10 @@ var geoFire = new GeoFire(transitFirebaseRef.child("_geofire"));
 // Kosong
 
 // Create a new GeoQuery instance
-var geoQuery = geoFire.query({
-  center: center,
-  radius: radiusInKm
-});
+// var geoQuery = geoFire.query({
+//   center: center,
+//   radius: radiusInKm
+// });
 
 /*****************/
 /*  GOOGLE MAPS  */
@@ -39,10 +34,50 @@ function initializeMap() {
   // Create the Google Map
   map = new google.maps.Map(document.getElementById("map-canvas"), {
     center: loc,
-    zoom: 5,
+    zoom: 3,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoomControl: true,
   });
 
   map.data.loadGeoJson('datasets/kacanghijau-2013.geojson');
+
+  var myLatlng = new google.maps.LatLng(-6,141.044922);
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      draggable:true,
+      title:"Drag me!",
+      animation: google.maps.Animation.DROP,
+      icon: 'marker.png'
+  });
+
+  google.maps.event.addListener(marker, 'dragend', function (event) {
+    document.getElementById("latbox").value = this.getPosition().lat();
+    console.log(this.getPosition().lat())
+    document.getElementById("lngbox").value = this.getPosition().lng();
+  });
+
 }
+
+var sidebar = new Vue({
+  el: '#sidebar',
+  data: {
+    message: 'Hello Vue.js!',
+    locations: [
+      { nama_provinsi: 'Add some todos' }
+    ]
+  },
+  methods: {
+    addLocation: function () {
+      console.log(this.nama)
+      console.log(this.latitude)
+      var nama = this.nama.trim()
+      var latitude = this.latitude.trim()
+      var longitude = this.longitude.trim()
+      this.locations.push({ nama_provinsi: nama, latitude: latitude, longitude: longitude })
+    },
+    removeLocation: function (index) {
+      this.todos.splice(index, 1)
+    }
+  }
+})
