ec9641f33d33fe4e59a24aefb06cd22677c603ed
Rizky Luthfianto <mrluthfianto@gmail.com>
2015-12-20 23:21:31 +0700

    agak dirombak

--- js/sig.js
+++ js/sig.js
@@ -7,26 +7,11 @@ var center = [-6,120]
 var radiusInKm = 0.5;
 
 // Get a reference to the Firebase public transit open data set
-// var transitFirebaseRef = new Firebase("https://torrid-inferno-2232.firebaseio.com/")
+var ref = new Firebase("https://torrid-inferno-2232.firebaseio.com/")
 
-// Create a new GeoFire instance, pulling data from the public transit data
-// var geoFire = new GeoFire(transitFirebaseRef.child("_geofire"));
+// Create a new GeoFire instance, pulling data from the data
+var geoFire = new GeoFire(ref.child("_geofire"));
 
-/*************/
-/*  GEOQUERY */
-/*************/
-// Kosong
-
-// Create a new GeoQuery instance
-// var geoQuery = geoFire.query({
-//   center: center,
-//   radius: radiusInKm
-// });
-
-/*****************/
-/*  GOOGLE MAPS  */
-/*****************/
-/* Initializes Google Maps */
 function initializeMap() {
   // Get the location as a Google Maps latitude-longitude object
   var loc = new google.maps.LatLng(center[0], center[1]);
@@ -39,7 +24,7 @@ function initializeMap() {
     zoomControl: true,
   });
 
-  map.data.loadGeoJson('datasets/kacanghijau-2013.geojson');
+  // map.data.loadGeoJson('datasets/kacanghijau-2013.geojson');
 
   var myLatlng = new google.maps.LatLng(-6,141.044922);
   var marker = new google.maps.Marker({
@@ -52,9 +37,8 @@ function initializeMap() {
   });
 
   google.maps.event.addListener(marker, 'dragend', function (event) {
-    document.getElementById("latbox").value = this.getPosition().lat();
-    console.log(this.getPosition().lat())
-    document.getElementById("lngbox").value = this.getPosition().lng();
+    document.getElementById("latitude").value = this.getPosition().lat();
+    document.getElementById("longitude").value = this.getPosition().lng();
   });
 
 }
@@ -62,22 +46,52 @@ function initializeMap() {
 var sidebar = new Vue({
   el: '#sidebar',
   data: {
-    message: 'Hello Vue.js!',
-    locations: [
-      { nama_provinsi: 'Add some todos' }
-    ]
+    message: '',
+    locations: []
   },
   methods: {
+    initializeLocations: function(data){
+      this.locations=data
+    },
     addLocation: function () {
-      console.log(this.nama)
-      console.log(this.latitude)
       var nama = this.nama.trim()
-      var latitude = this.latitude.trim()
-      var longitude = this.longitude.trim()
+      var latitude = parseInt(document.getElementById("latitude").value)
+      var longitude = parseInt(document.getElementById("longitude").value)
+      console.log(longitude)
+
       this.locations.push({ nama_provinsi: nama, latitude: latitude, longitude: longitude })
+      ref.set(this.locations)
     },
     removeLocation: function (index) {
-      this.todos.splice(index, 1)
+      this.locations.splice(index, 1)
+      ref.set(this.locations)
     }
   }
 })
+
+var data_from_firebase;
+ref.on("value", function(snapshot) {
+  locations=snapshot.val()
+  sidebar.initializeLocations(locations)
+
+  var infowindow = new google.maps.InfoWindow();
+
+  var marker, i;
+
+  for (i = 0; i < locations.length; i++) {  
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
+      map: map
+    });
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infowindow.setContent('<b>'+locations[i].nama_provinsi+'</b><br/>Luas Panen: '+locations[i].luas_panen+'<br/>Produktivitas: '+locations[i].produktivitas+'<br/>Produksi: '+locations[i].produksi);
+        // infowindow.setMaxWidth(300);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+  }
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
